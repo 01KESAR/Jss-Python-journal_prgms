32c86ac329d49e60e095d5994853b2cefa33b267
KESAR BADI_01 <kesarbadi04@gmail.com>
2024-06-24 19:41:43 +0530

    Add files via upload

---
+++ JP1.py
@@ -0,0 +1,14 @@
+# Write a Python Prgm check if a number belongs to fibonacci sequences 
+
+n=int(input('enter the number'))
+f1=f3=0
+f2=1
+while f3<=n:
+    if f3==n:
+        print(n,'belongs to fibonacci sequences')
+        break
+    f3=f1+f2
+    f1=f299
+    f2=f3
+else:
+    print(n,'does not belong to fibonacci sequences')   
